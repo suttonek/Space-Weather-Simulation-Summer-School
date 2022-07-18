cf7e63f01c9149bb2f1af7f8bdb9ef319d390739
Eric Sutton <ericsutton@econ2-195-105-dhcp.int.colorado.edu>
2022-07-18 13:32:07 -0600

    DEV: Cosine approximation via Taylor expansion

--- day_1/cos_approx.py
+++ day_1/cos_approx.py
@@ -3,17 +3,22 @@
 
 cosine approximation function
 """
-__author__ = 'Qusai Al Shidi'
-__email__ = 'qusai@umich.edu'
+__author__ = 'Eric Sutton'
+__email__ = 'eric.sutton@colorado.edu'
 
 from math import factorial
 from math import pi
 
 
 def cos_approx(x, accuracy=10):
+    """Returns an approximation to the cos(x) function based on the Taylor 
+    expansion sum( (-1)^n * x^(2*n) / (2*n)! ), where n is the total number of 
+    expansion terms
     """
-    """
-    return 
+    # Generate individual terms of the Taylor expansion
+    summands = [(-1)**n*x**(2*n)/factorial(2*n) for n in range(accuracy+1)]
+    # Return the sum of the Taylor expansion terms
+    return sum(summands)
 
 
 
